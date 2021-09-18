000ba995a063dafed056dc04dc39d3265451b8c8
mill3na <58698807+mill3na@users.noreply.github.com>
2021-09-17 22:02:44 -0300

    Create CircunferenceCalc.js

---
+++ CircunferenceCalc.js
@@ -0,0 +1,14 @@
+
+const PI = 3.14
+
+let raio = 10
+
+//you can use your PI
+
+// var circunferencia = (PI * (raio **2))
+
+//or you can use the Math.PI value
+
+var circunferencia = (Math.PI * (raio **2))
+
+console.log('A circunferência vale: '+ circunferencia +' m2.')
