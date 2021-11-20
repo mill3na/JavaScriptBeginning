9a4720945ca67b858d39ed15c10cf68e4eb002ed
milenaProfissional <maiaaraujo.milena@gmail.com>
2021-11-20 00:36:25 -0300

    estrutura de controle if - parte 1 e 2

--- Estruturas de controle/2_if.js
+++ Estruturas de controle/2_if.js
@@ -17,3 +17,16 @@ function teste2(numero){
 }
 teste2(6)
 teste2(8)
+// imprime 6 e o 8. O ; separou os blocos, então o console.log() foi executado independente do código. É como se estivesse assim: 
+
+
+function teste2(numero){
+    if (numero > 7)
+    ; // bloco de código vazio
+    // não usar ; com as estruturas de controle!
+    
+    {
+        console.log(numero)
+
+    }
+}
